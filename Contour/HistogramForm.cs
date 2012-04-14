543b9128eccf57d34e44d7c68b8a78a6e7c48d78
Pavel Egorov <xoposhiy@mail.ru>
2012-04-14 13:49:04 +0600

    Rename forms

---
+++ Contour/HistogramForm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Contour
+{
+    public partial class HistogramForm : Form
+    {
+        public HistogramForm(IEnumerable<Rectangle> boxes)
+        {
+            InitializeComponent();
+            hist.Titles.Add("Колличество — Диаметр");
+
+            IEnumerable<KeyValuePair<int, int>> data = boxes
+                .GroupBy(Util.Diameter)
+                .ToDictionary(grp => grp.Key, grp => grp.Count())
+                .OrderByDescending(entry => entry.Key)
+                .Where(entry => entry.Key < 100);
+
+            var ser = new Series();
+            foreach (var dataEntry in data)
+                ser.Points.AddXY(dataEntry.Key, dataEntry.Value);
+            hist.Series.Clear();
+            hist.Series.Add(ser);
+        }
+    }
+}
