f88f8fd7b1abe8264aa9f0defdc888dd6a98b1bf
mkozhevnikov <mak.egik@gmail.com>
2013-04-25 01:04:24 +0600

    Rotate image to avg skew of filtered lines

---
+++ Orient/Algorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orient
+{
+    class Algorithm
+    {
+        public Algorithm(string file) {
+            var state = new MainFormState(file);
+            if (!state.Criteria90()) {
+                state.Dispose();
+                state = new MainFormState(state.Rotate());
+            }
+            var skew = state.FilteredLines.Average(line => line.LinearRegression().Skew());
+            state.Dispose();
+            state = new MainFormState(state.Rotate(skew));
+        }
+    }
+}
