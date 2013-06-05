c26b2b0ae6a004699b41e4d2b194c3b3931fb6d1
mkozhevnikov <mak.egik@gmail.com>
2013-06-06 03:31:11 +0600

    Patterns testing(with quotation). Pattern Diagram in xls

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,32 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Mime;
 using System.Reflection;
-using System.Text;
 using Orient;
 
 namespace Testing
 {
 	class Program
 	{
-		private static readonly string TestBase = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) +
-										  @"\..\..\base\more";
+        private static readonly string AssemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        private static readonly string TestBase = AssemblyPath + @"\..\..\base";
+	    private static readonly string Output = AssemblyPath + @"\..\..\Contour\Resources\pattern.csv";
 
-		static void Main(string[] args)
+	    static void Main(string[] args)
 		{
-//			Run180Criteria(Directory.GetDirectories(TestBase).SelectMany(Directory.GetFiles).ToArray());
-			Run180Criteria(Directory.GetFiles(TestBase));
+			Run180Criteria2(Directory.GetDirectories(TestBase).SelectMany(Directory.GetFiles).ToArray());
+//			Run180Criteria(Directory.GetFiles(TestBase));
 		}
 
 		private static void Run180Criteria(string[] files)
 		{
 			int ok = 0;
 			int wrong = 0;
-			try
-			{
-				foreach (var file in files)
+			try {
+                foreach (var file in files)
 				{
 					Console.Write(file.PadRight(60, ' ') + "  ");
 					var state = new MainFormState(file);
@@ -50,19 +47,24 @@ namespace Testing
 		{
 			try
 			{
-				foreach (var file in files)
+                var writer = new StreamWriter(Output, false);
+                foreach (var file in files)
 				{
 					var state = new MainFormState(file);
-					var orient = new Orient.Orient(state);
+					var orient = new AlgorithmExecutor(state);
 					var punctOk = orient.CountPattern(orient.CountPunctuation);
 					var upperOk = orient.CountPattern(orient.CountUpperCase);
+				    var quatOk = orient.CountPattern(orient.CountQuotations);
 					state = new MainFormState(file);
 					state.Rotate(180);
-					orient = new Orient.Orient(state);
+					orient = new AlgorithmExecutor(state);
 					var punctBad = orient.CountPattern(orient.CountPunctuation);
 					var upperBad = orient.CountPattern(orient.CountUpperCase);
+                    var quatBad = orient.CountPattern(orient.CountQuotations);
 
-					Console.WriteLine("{0};{1};{2};{3};{4}", punctOk, punctBad, upperOk, upperBad, file);
+				    var result = string.Format("{0};{1};{2};{3};{4};{5};{6}", punctOk, punctBad, upperOk, upperBad, quatOk, quatBad, file);
+				    writer.WriteLine(result);
+                    Console.WriteLine(result);
 				}
 			}
 			catch (Exception ex)
